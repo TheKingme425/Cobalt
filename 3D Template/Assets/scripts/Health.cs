fe80be2aebdf6f73b137793c4042785feaf301bd
MUKILTEO\1554910 <1554910@mukilteo.wednet.edu>
2025-04-18 11:48:39 -0700

    health script

---
+++ 3D Template/Assets/scripts/Health.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health, maxHealth;
+    public static event Action OnPlayerDamaged;
+    public static event Action OnPlayerDeath;
+    public Image healthBar;
+
+
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        health = maxHealth;
+    }
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        OnPlayerDamaged?.Invoke();
+
+        if (health <= 0)
+        {
+
+        }
+    }
+}
