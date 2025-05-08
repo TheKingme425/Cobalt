231d8564bc93e32c545784cbf9365c4358e72b62
MUKILTEO\1717487 <fgreenmun@gmail.com>
2025-05-08 12:56:38 -0700

    ooga chaka ooga chaka

--- 3D Template/Assets/scripts/Health.cs
+++ 3D Template/Assets/scripts/Health.cs
@@ -32,6 +32,8 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(GameObject.FindGameObjectWithTag("Player"));
+            GameObject.Find("DeathScreen").SetActive(true);
+            GameObject.Find("Canvas").SetActive(false);
         }
     }
 }
