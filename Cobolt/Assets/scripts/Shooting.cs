38750e7f0ffa7cdba3b3813aa49c5efaffe910d3
fgreenmun@gmail.com <fgreenmun@gmail.com>
2025-05-10 15:52:18 -0700

    fghh

---
+++ Cobolt/Assets/scripts/Shooting.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform FirePoint;
+    public LineRenderer lineRenderer; 
+    public float laserLength = 10f;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            shooting();
+        }
+    }
+
+    public void shooting()
+    {
+        RaycastHit hit;
+        lineRenderer.enabled = true;
+        Invoke(nameof(Disappear), 1);
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            //Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * hit.distance, Color.red, 1);
+            lineRenderer.SetPosition(0, FirePoint.position);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            // If the raycast doesn't hit anything, extend the laser to its maximum length
+            lineRenderer.SetPosition(0, FirePoint.position);
+            lineRenderer.SetPosition(1, FirePoint.position + FirePoint.forward * laserLength);
+        }
+    }
+
+    public void Disappear()
+    {
+        lineRenderer.enabled = false;
+    }
+}
